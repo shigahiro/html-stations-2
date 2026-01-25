d547855abd030d9bbd2b8bc863c0cb247f71b6f1
shigahiro <hiroki2131@icloud.com>
2026-01-25 10:20:30 +0900

    ステーション13クリア

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,11 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
+  const check = document.querySelector("#check");
+  const text = document.querySelector("#text");
 
+  if (check.checked) {
+    text.style.backgroundColor = "red";
+  } else {
+    text.style.backgroundColor = "white";
+  }
 }
